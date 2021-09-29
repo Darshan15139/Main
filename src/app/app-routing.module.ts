dcba6d9401f4edd863369fc2cd3d0ee38fd0b100
Darshan15139 <dvc3199@gmail.com>
2021-09-29 15:52:08 +0530

    Good to go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
 import { ProductTabComponent } from './Components/product-tab/product-tab.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
 import { CartComponent } from './Components/cart/cart.component';
 import { CheckoutComponent } from './Components/checkout/checkout.component';
 import { PageLayoutComponent } from './Components/page-layout/page-layout.component';
 const routes: Routes = [
-  {path: 'Home',component: PageLayoutComponent},
-  {path: 'head', component: HeaderComponent},
-  {path: 'foot', component: FooterComponent},
-  {path: '', component: ProductTabComponent},
-  {path: '', component: SignUpComponent},
-  {path: '', component: CartComponent},
-  {path: '', component: CheckoutComponent}
+  {path: '',component: PageLayoutComponent},
+  // {path: 'head', component: HeaderComponent},
+  // {path: 'foot', component: FooterComponent},
+  {path: '', redirectTo:'page-layout', pathMatch:'full'},
+
+  {path: 'product-tab', component: ProductTabComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent}
 ];
 
 @NgModule({
